a3083d24d4cfa1276418c756d046e3017980be52
Rob Armitage <robert.c.armitage@gmail.com>
2019-11-03 14:46:48 +0000

    add spring boot files to server folder

---
+++ server/newsbackend/src/main/java/com/codeclan/example/newsbackend/NewsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.newsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsbackendApplication.class, args);
+	}
+
+}
---
+++ server/newsbackend/src/test/java/com/codeclan/example/newsbackend/NewsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.newsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
