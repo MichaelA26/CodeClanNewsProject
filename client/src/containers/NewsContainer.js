09b50855ce8a99b1d51ecde68cabd48c55c2aef9
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-11-04 14:14:53 +0000

    routes and paths

--- client/src/containers/NewsContainer.js
+++ client/src/containers/NewsContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import HomePage from "../components/HomePage"
 import ArticlesComponent from '../components/ArticlesComponent'
 import JournalistComponent from '../components/JournalistComponent'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 class NewsContainer extends React.Component {
   constructor(props) {
@@ -58,11 +60,22 @@ class NewsContainer extends React.Component {
 
     render(){
       return(
-        <div>
-        <h1>Joke News Inc. "Is it a joke, or is it a weird truth..."</h1>
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" component={HomePage} />
+            <Route 
+              path="/articles" 
+              render={() => <ArticlesComponent articles={this.state.articles}/>}
+              />
+            <Route 
+              path="/journalists" 
+              render={() => <JournalistComponent journalists={this.state.journalists} /> }
+              />
+          </React.Fragment>
+        </Router>
+        /* <h1>Joke News Inc. "Is it a joke, or is it a weird truth..."</h1>
         <ArticlesComponent articles={this.state.articles}/>
-        <JournalistComponent journalists={this.state.journalists}/>
-        </div>
+        <JournalistComponent journalists={this.state.journalists}/> */
       );
     }
   }
