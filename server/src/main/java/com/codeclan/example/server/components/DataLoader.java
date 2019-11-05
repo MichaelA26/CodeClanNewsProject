c1fa50f49fcf9e17477a57ab2199ba4a26b2d34c
Chris Haining <chrishaining@yahoo.co.uk>
2019-11-05 09:40:32 +0000

    removes overlong articles

--- server/src/main/java/com/codeclan/example/server/components/DataLoader.java
+++ server/src/main/java/com/codeclan/example/server/components/DataLoader.java
@@ -80,23 +80,23 @@ public class DataLoader implements ApplicationRunner {
     "Science");
     articleRepository.save(article3);
 
-    Article article4 = new Article(
-    "Life Change",
-    "29/10/2019",
-    journalist2,
-    "It's been a while coming but I might as well just say it... I am just too famous for you. Too gosh darn famous. You think all this beauty and intelijence comes for free? NO! I officially revoke your right to talk to me unless you're famous. GET ME BACK INTO THE LIMELIGHT DAMMIT!",
-    "An important notice for my fans.",
-    "Celebrity");
-    articleRepository.save(article4);
-
-    Article article5 = new Article(
-    "UPDATE: Slughorn",
-    "20/08/2019",
-    journalist3,
-    "Hello Pottermore fans! A young Voldemort once asked Slughorn what a horcrux is. It was not disclosed in the books, but the horcrux in questin was in fact a super top secret horcrux written in the depths of JavaScript. Only a true potions master/software developer like Slughorn would know the truth.",
-    "The secret horcrux.",
-    "Entertainment & Arts");
-    articleRepository.save(article5);
+//    Article article4 = new Article(
+//    "Life Change",
+//    "29/10/2019",
+//    journalist2,
+//    "It's been a while coming but I might as well just say it... I am just too famous for you. Too gosh darn famous. You think all this beauty and intelijence comes for free? NO! I officially revoke your right to talk to me unless you're famous. GET ME BACK INTO THE LIMELIGHT DAMMIT!",
+//    "An important notice for my fans.",
+//    "Celebrity");
+//    articleRepository.save(article4);
+//
+//    Article article5 = new Article(
+//    "UPDATE: Slughorn",
+//    "20/08/2019",
+//    journalist3,
+//    "Hello Pottermore fans! A young Voldemort once asked Slughorn what a horcrux is. It was not disclosed in the books, but the horcrux in questin was in fact a super top secret horcrux written in the depths of JavaScript. Only a true potions master/software developer like Slughorn would know the truth.",
+//    "The secret horcrux.",
+//    "Entertainment & Arts");
+//    articleRepository.save(article5);
 
     Article article6 = new Article(
     "UPDATE: Harry Potter's father",
@@ -107,23 +107,7 @@ public class DataLoader implements ApplicationRunner {
     "Entertainment & Arts");
     articleRepository.save(article6);
 
-    Article article7 = new Article(
-    "Soup-reme Court goes pot-ty",
-    "27/08/2019",
-    journalist4,
-    "The Supreme Court today banned all forms of soup, following a spate of soup-related crime. Since July, there have been 43 separate soup-related crimes across the United States. It all began with the Battle of Minestrone in Seattle, in which 12 people were killed. According to a government spokesperson, 'America has declared a war on soup. Soup is destroying our communities, and today's decision will lead to a safer world.",
-    "USA's top judges ban soup.",
-    "Crime");
-    articleRepository.save(article7);
 
-    Article article8 = new Article(
-    "Aldi sues Soup-reme Court",
-    "02/09/2019",
-    journalist4,
-    "Shortly after the Soup-reme Court initiated the soup ban, they legally changed the name of the Supreme Court to 'The Soup-reme Court'. The owners of Aldi's own brand soup (Soup-reme) has filed a lawsuit against the Soup-reme Court to try and bring the name of the 'Soup-Reme Court' back to 'Soup-reme' soup. The case rages on...",
-    "Spicy court case rages on as Aldi seeks name reclamation",
-    "Crime");
-    articleRepository.save(article8);
 
     Article article9 = new Article(
     "How dare you!",
@@ -143,14 +127,14 @@ public class DataLoader implements ApplicationRunner {
     "Technology");
     articleRepository.save(article10);
 
-    Article article11 = new Article(
-    "A Great Disturbance",
-    "11/03/2019",
-    journalist6,
-    "I felt a great disturbance in the Force, as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened.",
-    "In my latest meditations I realised that something was not right.",
-    "Crime");
-    articleRepository.save(article11);
+//    Article article11 = new Article(
+//    "A Great Disturbance",
+//    "11/03/2019",
+//    journalist6,
+//    "I felt a great disturbance in the Force, as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened.",
+//    "In my latest meditations I realised that something was not right.",
+//    "Crime");
+//    articleRepository.save(article11);
 
     Article article12 = new Article(
     "Journey's on Utapau.",
@@ -170,49 +154,5 @@ public class DataLoader implements ApplicationRunner {
     "Sport");
     articleRepository.save(article13);
 
-    Article article14 = new Article(
-    "ESports are Real Sports too 2",
-    "19/06/2019",
-    journalist7,
-    "Hi it's Jeff from the Overwatch team. A quick update regarding my article 'ESports are Real Sports too?'. Are ESorts sports too? No. A sport involves exercise. But if we tell enough parents that ESports ARE sports a second time then think of all the extra video games we could sell MWA-HA-HA-HA-HA!!!!!",
-    "The second greatest conundrum of 2019, are ESports Sports 2?",
-    "Sport");
-    articleRepository.save(article14);
-
-    Article article15 = new Article(
-    "Thinking Ahead to Hogmanay Without Me",
-    "01/04/2019",
-    journalist8,
-    "Imagine a time, where Hogmanay did not star myself. I was beamed into nearly every house across the country as the bells rang to signal another trip around the Sun. Well this is about to happen, on the eve of 2020! It will no doubt be much worse without me, but hopefully it will give us a chance to celebrate my legacy.",
-    "Dame Jackie Bird reflects on her time hosting every Hogmanay in living memory.",
-    "Entertainment & Arts");
-    articleRepository.save(article15);
-
-    Article article16 = new Article(
-    "My Last Article was not an April Fool's Joke",
-    "31/10/2019",
-    journalist8,
-    "I failed to realise until now that my last article was published on April Fool's day. I am not impressed that nobody has raised this point until now that I actually will not be hosting another Hogmanay programme. It is almost as if nobosy cares.. As if I passed on like a fart in the wind... I'll be back.",
-    "Jackie clears up some confusion regarding an article published earlier this year.",
-    "Entertainment & Arts");
-    articleRepository.save(article16);
-
-    Article article17 = new Article(
-    "The Fellowship is Formed!",
-    "14/03/2019",
-    journalist9,
-    "A coucil of men, elves and dwarves were summoned to Rivendell to discuss how to defeat the threat of Sauron. One lucky warrior was to be picked to carry the burden of the One Ring to Mordor and destroy it in the fires of Mount Doom. But instead of selecting a brave, successful, corageous, handsome, humble, romantic, brilliant, unique, driven, qualified, amazing, incredible, unparralleled, manly, heroic leader of men like me. They chose the Hobbit Frodo Baggins. Lame.",
-    "The Dark Lord Sauron has waged war on Middle Earth, the realm of the living retaliate.",
-    "Crime");
-    articleRepository.save(article17);
-
-    Article article18 = new Article(
-    "Earth May Be Flat. Middle Earth However...",
-    "24/07/2019",
-    journalist9,
-    "As it transpires, Middle Earth is fictional. But given how even fantastical creatures can operate as if a force of gravity is acting upon them, Gandalf the Grey has deduces that Middle Earth is in fact spherical! As all worlds are. Even real ones he says... Please respond if you are a 'Flat-Earther' whom is offended by this revelation.",
-    "Some commentary on the recent phenominon of 'Flat-Earthers'.",
-    "Science");
-    articleRepository.save(article18);
   }
 }
