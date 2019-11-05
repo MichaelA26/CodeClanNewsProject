18d24c2e63390474c1233e5c9ec09479bfc8fb55
Michael Alexander <michael_alexander_20@outlook.com>
2019-11-05 12:15:07 +0000

    stories fit into the code now

--- server/src/main/java/com/codeclan/example/server/components/DataLoader.java
+++ server/src/main/java/com/codeclan/example/server/components/DataLoader.java
@@ -80,23 +80,23 @@ public class DataLoader implements ApplicationRunner {
     "Science");
     articleRepository.save(article3);
 
-//    Article article4 = new Article(
-//    "Life Change",
-//    "29/10/2019",
-//    journalist2,
-//    "It's been a while coming but I might as well just say it... I am just too famous for you. Too gosh darn famous. You think all this beauty and intelijence comes for free? NO! I officially revoke your right to talk to me unless you're famous. GET ME BACK INTO THE LIMELIGHT DAMMIT!",
-//    "An important notice for my fans.",
-//    "Celebrity");
-//    articleRepository.save(article4);
-//
-//    Article article5 = new Article(
-//    "UPDATE: Slughorn",
-//    "20/08/2019",
-//    journalist3,
-//    "Hello Pottermore fans! A young Voldemort once asked Slughorn what a horcrux is. It was not disclosed in the books, but the horcrux in questin was in fact a super top secret horcrux written in the depths of JavaScript. Only a true potions master/software developer like Slughorn would know the truth.",
-//    "The secret horcrux.",
-//    "Entertainment & Arts");
-//    articleRepository.save(article5);
+    Article article4 = new Article(
+    "Life Change",
+    "29/10/2019",
+    journalist2,
+    "It's been a while coming but I might as well just say it... I am just too famous for you. Too gosh darn famous. You think all this beauty and intelijence comes for free? NO! I officially revoke your right to talk to me unless you're famous.",
+    "An important notice for my fans.",
+    "Celebrity");
+    articleRepository.save(article4);
+
+    Article article5 = new Article(
+    "UPDATE: Slughorn",
+    "20/08/2019",
+    journalist3,
+    "Hello Pottermore fans! A young Voldemort once asked Slughorn what a horcrux is. The horcrux in question was actually a top secret horcrux written JavaScript. Only a true potions master/software developer like Slughorn would know the truth.",
+    "The secret horcrux.",
+    "Entertainment & Arts");
+    articleRepository.save(article5);
 
     Article article6 = new Article(
     "UPDATE: Harry Potter's father",
@@ -107,7 +107,23 @@ public class DataLoader implements ApplicationRunner {
     "Entertainment & Arts");
     articleRepository.save(article6);
 
+    Article article7 = new Article(
+            "Soup-reme Court goes pot-ty",
+            "27/08/2019",
+            journalist4,
+            "The Supreme Court today banned all forms of soup, following a spate of soup-related crime. Since July, there have been 43 separate soup-related crimes. It all began with the Battle of Minestrone in Seattle, in which 12 people were killed.",
+            "USA's top judges ban soup.",
+            "Crime");
+    articleRepository.save(article7);
 
+    Article article8 = new Article(
+            "Aldi sues Soup-reme Court",
+            "02/09/2019",
+            journalist4,
+            "After the Soup-reme Court initiated the soup ban, they legally changed the name of the Supreme Court to 'The Soup-reme Court'. Aldi's own brand soup (Soup-reme) filed a lawsuit to try and bring the name of the 'Soup-Reme Court' back to 'Soup-reme' soup.",
+            "Spicy court case rages on as Aldi seeks name reclamation",
+            "Crime");
+    articleRepository.save(article8);
 
     Article article9 = new Article(
     "How dare you!",
@@ -127,14 +143,14 @@ public class DataLoader implements ApplicationRunner {
     "Technology");
     articleRepository.save(article10);
 
-//    Article article11 = new Article(
-//    "A Great Disturbance",
-//    "11/03/2019",
-//    journalist6,
-//    "I felt a great disturbance in the Force, as if millions of voices suddenly cried out in terror and were suddenly silenced. I fear something terrible has happened.",
-//    "In my latest meditations I realised that something was not right.",
-//    "Crime");
-//    articleRepository.save(article11);
+    Article article11 = new Article(
+    "A Great Disturbance",
+    "11/03/2019",
+    journalist6,
+    "I felt a great disturbance in the Force, as if millions of voices suddenly cried out in terror and were suddenly silenced.",
+    "Something terrible has happened.",
+    "Crime");
+    articleRepository.save(article11);
 
     Article article12 = new Article(
     "Journey's on Utapau.",
@@ -153,6 +169,51 @@ public class DataLoader implements ApplicationRunner {
     "The greatest conundrum of 2019, are ESports Sports?",
     "Sport");
     articleRepository.save(article13);
+
+    Article article14 = new Article(
+            "ESports are Real Sports too 2",
+            "19/06/2019",
+            journalist7,
+            "A quick update regarding my article 'ESports are Real Sports too?'. We are developing a game which simulates sports, to further the illusion of exercise!",
+            "The second greatest conundrum of 2019, are ESports Sports 2?",
+            "Sport");
+    articleRepository.save(article14);
+
+    Article article15 = new Article(
+            "Thinking Ahead to Hogmanay Without Me",
+            "01/04/2019",
+            journalist8,
+            "Imagine a time, where Hogmanay did not star me. I was beamed into nearly every house across the country as the bells rang to signal another trip around the Sun. Well this is about to happen, on the eve of 2020! It will be worse now.",
+            "Dame Jackie Bird reflects on her time hosting every Hogmanay in living memory.",
+            "Entertainment & Arts");
+    articleRepository.save(article15);
+
+    Article article16 = new Article(
+            "My Last Article was not an April Fool's Joke",
+            "31/10/2019",
+            journalist8,
+            "I failed to realise that my last article was published on April Fool's day. I am not impressed that nobody has raised this point until now that this was not fake news. It's as if I passed on like a fart in the wind.",
+            "Jackie clears up some confusion regarding an article published earlier this year.",
+            "Entertainment & Arts");
+    articleRepository.save(article16);
+
+    Article article17 = new Article(
+            "The Fellowship is Formed!",
+            "14/03/2019",
+            journalist9,
+            "A coucil of warriors were summoned to Rivendell to discuss how to defeat Sauron. One warrior was to be picked to carry the of the One Ring to Mordor. But instead of selecting a brave, handsome, humble, heroic leader like me, they chose Frodo.",
+            "The Dark Lord Sauron has waged war on Middle Earth, the realm of the living retaliate.",
+            "Crime");
+    articleRepository.save(article17);
+
+    Article article18 = new Article(
+            "Earth May Be Flat. Middle Earth However...",
+            "24/07/2019",
+            journalist9,
+            "As it transpires, Middle Earth is fictional. But given how even fantastical creatures can operate as if a force of gravity is acting upon them, Gandalf the Grey has deduces that Middle Earth is in fact spherical! As all worlds are. Even real ones he says.",
+            "Some commentary on the recent phenomenon of 'Flat-Earthers'.",
+            "Science");
+    articleRepository.save(article18);
 
   }
 }
