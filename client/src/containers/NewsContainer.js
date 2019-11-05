1a58394e7e432fb9e650c7cd40780ea0ea5daf3a
Michael Alexander <michael_alexander_20@outlook.com>
2019-11-05 10:33:34 +0000

    changed error message picture

--- client/src/containers/NewsContainer.js
+++ client/src/containers/NewsContainer.js
@@ -12,57 +12,17 @@ class NewsContainer extends React.Component {
 
     this.state = {
       articles: [
-        // {
-        // id: 1,
-        // headline: "Cookie Monster Crisis!",
-        // summary: "Cookie Monster falls ill after big feast.",
-        // date: "03/11/2019",
-        // story: "After partaking in a glorious cookie bonanza, poor ol' cookie monster discovers that he is gluten intolerant. The aftermath was... Messy. You've got no fans.",
-        // category: "Celebrity",
-        // journalist: {
-        //   id: "1",
-        //   firstName: "Weildstone",
-        //   lastName: "Raider",
-        //   phoneNumber: "080000 1066",
-        //   employed: "Yes"}
-        // },
-        // {
-        //   id: 2,
-        //   headline: "The Sinking City.",
-        //   summary: "Man trying to break World Record weight creates sinkhole",
-        //   date: "04/11/2019",
-        //   story: "Noel Edmunds has changed career and was attempting to become the worlds heaviest man. After indulging in snacks for over a yesar his weight grew so colossal that he inadvertantly created a sinkhole. He was swallowed up by the earth and is expected to hit the core of the Earth next Tuesday. Deal or no deal indeed.",
-        //   category: "Health",
-        //   journalist: {
-        //     id: "2",
-        //     firstName: "Barry",
-        //     lastName: "Trotter",
-        //     phoneNumber: "01506 123456",
-        //     employed: "Yes"}
-        //   }
+
         ],
 
         journalists: [
-          // {
-          //   id: 1,
-          //   firstName: "Weildstone",
-          //   lastName: "Raider",
-          //   phoneNumber: "080000 1066",
-          //   employed: "Yes"
-          // },
-          // {
-          //   id: 2,
-          //   firstName: "Barry",
-          //   lastName: "Trotter",
-          //   phoneNumber: "01506 123456",
-          //   employed: "Yes"
-          // }
+
         ]
       }
     }
 
     componentDidMount(){
-    
+
       const promises = [
       fetch('http://localhost:8080/articles')
       .then(res => res.json())
@@ -88,12 +48,12 @@ class NewsContainer extends React.Component {
             "Is it a joke, or is it a weird truth..." </h1>
             <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route 
-              path="/articles" 
+            <Route
+              path="/articles"
               render={() => <ArticlesComponent articles={this.state.articles}/>}
               />
-            <Route 
-              path="/journalists" 
+            <Route
+              path="/journalists"
               render={() => <JournalistComponent journalists={this.state.journalists} /> }
               />
             <Route component={ErrorPage} />
